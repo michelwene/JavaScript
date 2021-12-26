6b380959cacb7aece1a8477c9632f1209f0ffe77
Michel Wene <michelwene@hotmail.com>
2021-12-26 16:48:09 -0300

    Add date on the list

--- projeto_inicial/main.js
+++ projeto_inicial/main.js
@@ -2,29 +2,41 @@
 import BotaoConclui from "../components/conclui-Tarefa.js";
 import BotaoDeleta from "../components/deleta-Tarefa.js";
 
-const criarTarefa = (evento) => {
+const handleNovoItem = (evento) => {
   evento.preventDefault();
-
   const lista = document.querySelector("[data-list]");
   const input = document.querySelector("[data-form-input]");
   const valor = input.value;
 
   const calendario = document.querySelector("[data-form-date]");
   const data = moment(calendario.value);
-  console.log(data.format("DD/MM/YYYY, h:mm:ss a"));
+
+  const dataFormatada = data.format("DD/MM/YYYY, h:mm:ss");
+
+  const dados = {
+    valor,
+    dataFormatada,
+  };
+  
+  const criaTarefa = criarTarefa(dados);
+  lista.appendChild(criaTarefa);
+
+  input.value = " ";
+};
+
+const criarTarefa = ({ valor, dataFormatada }) => {
   const tarefa = document.createElement("li");
   tarefa.classList.add("task");
-  const conteudo = `<p class="content">${valor}</p>`;
+  const conteudo = `<p class="content">${dataFormatada} * ${valor} </p>`;
 
   tarefa.innerHTML = conteudo;
 
   tarefa.appendChild(BotaoConclui());
   tarefa.appendChild(BotaoDeleta());
-  lista.appendChild(tarefa);
 
-  input.value = " ";
+  return tarefa;
 };
 
 const novaTarefa = document.querySelector("[data-form-button]");
 
-novaTarefa.addEventListener("click", criarTarefa);
+novaTarefa.addEventListener("click", handleNovoItem);
